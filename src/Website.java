e38da77bac1a5729966d5e530c6247814817da0c
Herculano Lima <hercullanolima@gmail.com>
2023-06-24 16:58:55 -0300

    Colocando o menu em um laço

---
+++ src/Website.java
@@ -0,0 +1,23 @@
+public class Website {
+    private String nome;
+    private String url;
+
+    public Website() {
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
