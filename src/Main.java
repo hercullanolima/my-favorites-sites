a6c53f36652f4acf500131d2318825018e1443aa
Herculano Lima <hercullanolima@gmail.com>
2023-06-24 16:10:27 -0300

    Permitir cadastro de sites

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("------ MEUS SITES FAVORITOS ------");
+
+        Scanner scanner = new Scanner(System.in);
+        List<Website> meussWebsites = new ArrayList<>();
+
+        String nome = scanner.nextLine();
+        String url = scanner.nextLine();
+
+        Website website = new Website();
+        website.setNome(nome);
+        website.setUrl(url);
+
+        meussWebsites.add(website);
+
+
     }
 }
